e9241f7786e722329055982e6852b268d2fba91d
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-30 16:46:20 +0100

    format on save

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,18 +1,18 @@
-import React, { useContext } from 'react';
-import { StyleSheet } from 'react-native';
-import { Context } from '../context/BlogContext';
+import React, {useContext} from 'react';
+import {StyleSheet} from 'react-native';
+import {Context} from '../context/BlogContext';
 import BlogPostForm from '../components/BlogPostForm';
 
-const CreateScreen = ({ navigation }) => {
-  const { addBlogPost } = useContext(Context);
+const CreateScreen = ({navigation}) => {
+    const {addBlogPost} = useContext(Context);
 
-  return (
-    <BlogPostForm
-      onSubmit={(title, content) => {
-        addBlogPost(title, content, () => navigation.navigate('Index'));
-      }}
-    />
-  );
+    return (
+        <BlogPostForm
+            onSubmit={(title, content) => {
+                addBlogPost(title, content, () => navigation.navigate('Index'));
+            }}
+        />
+    );
 };
 
 const styles = StyleSheet.create({});
