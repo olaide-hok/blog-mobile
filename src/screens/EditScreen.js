bb5578c96a8c461d8a769940d08f3e6924c05bd1
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-30 16:45:57 +0100

    format on save

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,22 +1,22 @@
-import React, { useContext } from 'react';
-import { StyleSheet } from 'react-native';
-import { Context } from '../context/BlogContext';
+import React, {useContext} from 'react';
+import {StyleSheet} from 'react-native';
+import {Context} from '../context/BlogContext';
 import BlogPostForm from '../components/BlogPostForm';
 
-const EditScreen = ({ navigation }) => {
-  const id = navigation.getParam('id');
-  const { state, editBlogPost } = useContext(Context);
+const EditScreen = ({navigation}) => {
+    const id = navigation.getParam('id');
+    const {state, editBlogPost} = useContext(Context);
 
-  const blogPost = state.find(blogPost => blogPost.id === id);
+    const blogPost = state.find((blogPost) => blogPost.id === id);
 
-  return (
-    <BlogPostForm
-      initialValues={{ title: blogPost.title, content: blogPost.content }}
-      onSubmit={(title, content) => {
-        editBlogPost(id, title, content, () => navigation.pop());
-      }}
-    />
-  );
+    return (
+        <BlogPostForm
+            initialValues={{title: blogPost.title, content: blogPost.content}}
+            onSubmit={(title, content) => {
+                editBlogPost(id, title, content, () => navigation.pop());
+            }}
+        />
+    );
 };
 
 const styles = StyleSheet.create({});
